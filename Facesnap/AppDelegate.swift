963026c8f0dbf301c9bd944ca3a0cf3b2cbb0c67
Muhammad Moaz <Moaz@Muhammads-MacBook-Pro.local>
2016-12-30 16:08:03 +0500

    Project setup

--- Facesnap/AppDelegate.swift
+++ Facesnap/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else { return false }
+        window.backgroundColor = .white
+        
+        let photoListVC = PhotoListVC()
+        let navigationController = UINavigationController(rootViewController: photoListVC)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
         return true
     }
 
