f6e58c76b1234138d319b4c825ac2e998845f27d
Muhammad Moaz <m-moaz@hotmail.com>
2016-12-31 05:05:45 +0500

    Adds PhotoFilterEdit VC

---
+++ Facesnap/PhotoFilterVC.swift
@@ -0,0 +1,83 @@
+//
+//  PhotoFilterVC.swift
+//  Facesnap
+//
+//  Created by Muhammad Moaz on 12/31/16.
+//  Copyright © 2016 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFilterVC: UIViewController {
+    
+    private var mainImage: UIImage
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var filterHeaderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Select a filter"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var filtersCollectionView: UICollectionView = {
+       let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.minimumInteritemSpacing = 1000
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    init(image: UIImage) {
+        self.mainImage = image
+        self.photoImageView.image = self.mainImage
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Layout
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(photoImageView)
+        
+        filterHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filterHeaderLabel)
+        
+        filtersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filtersCollectionView)
+        
+        NSLayoutConstraint.activate([
+            filtersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            filtersCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            filtersCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            filtersCollectionView.heightAnchor.constraint(equalToConstant: 200.0),
+            filtersCollectionView.topAnchor.constraint(equalTo: filterHeaderLabel.bottomAnchor),
+            filterHeaderLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            filterHeaderLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: filtersCollectionView.topAnchor),
+            photoImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            photoImageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            photoImageView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            ])
+    }
+
+}
