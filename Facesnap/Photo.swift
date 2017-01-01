b8fbf0a7e5955ac0b0d53f702dfe6f7c6d304cd2
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-01 12:38:21 +0500

    Adds convenience methods for location and tag entities

--- Facesnap/Photo.swift
+++ Facesnap/Photo.swift
@@ -6,11 +6,43 @@
 //  Copyright © 2017 Muhammad Moaz. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
+import CoreLocation
 
 class Photo: NSManagedObject {
+    static let entityName = String(describing: Photo.self)
     
+    class func photo(withImage image:UIImage) -> Photo {
+        let photo = NSEntityDescription.insertNewObject(forEntityName: Photo.entityName, into: CoreDataController.sharedInstance.managedObjectContext) as! Photo
+        photo.date = NSDate().timeIntervalSince1970
+        photo.image = UIImageJPEGRepresentation(image, 1.0)! as NSData
+        return photo
+    }
+    
+    class func photoWith(image: UIImage, tags: [String], location: CLLocation?) {
+        let photo = Photo.photo(withImage: image)
+        photo.addTags(tags: tags)
+        photo.addLocation(location: location)
+    }
+    
+    func addTag(withTitle title: String) {
+        let tag = Tag.tag(withTitle: title)
+        tags.insert(tag)
+    }
+    
+    func addTags(tags: [String]) {
+        for tag in tags {
+            addTag(withTitle: tag)
+        }
+    }
+    
+    func addLocation(location: CLLocation?) {
+        if let location = location {
+            let photoLocation = Location.locationWith(location.coordinate.latitude, longitude: location.coordinate.longitude)
+            self.location = photoLocation
+        }
+    }
 }
 
 extension Photo {
