101917b7f784f0d8fcac6d3ffd24a7fe10391b58
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-02 04:48:25 +0500

    Adds data source to SortableListVC

---
+++ Facesnap/SortableDataSource.swift
@@ -0,0 +1,67 @@
+//
+//  SortableDataSource.swift
+//  Facesnap
+//
+//  Created by Muhammad Moaz on 1/2/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+protocol CustomTitleConvertible {
+    var title: String { get }
+}
+
+extension Tag: CustomTitleConvertible {}
+
+class SortableDataSource<SortType: CustomTitleConvertible>: NSObject, UITableViewDataSource where SortType: NSManagedObject {
+    
+    private let fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>
+    
+    init(fetchRequest: NSFetchRequest<NSFetchRequestResult>, managedObjectContext moc: NSManagedObjectContext) {
+        self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        super.init()
+    }
+    
+    func executeFetch() {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch let error as NSError {
+            print("Unresolved Sortable Error \(error), \(error.userInfo)")
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return fetchedResultsController.fetchedObjects?.count ?? 0
+        default: return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "sortableItemCell")
+        cell.selectionStyle = .none
+        
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            cell.textLabel?.text = "All \(SortType.self)s"
+            cell.accessoryType = .checkmark
+        case (1, _):
+            guard let sortItem = fetchedResultsController.fetchedObjects?[indexPath.row
+                ] as? SortType else { break }
+            cell.textLabel?.text = sortItem.title
+        default:
+            break
+            
+        }
+        
+        return cell
+    }
+    
+}
